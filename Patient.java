9da2d0438086bfd4786b90a781510ed2c5b70ba9
Popuri Haritha <popuriharitha763@gmail.com>
2026-02-27 18:51:39 +0530

    Add Patient class with constructor

---
+++ Patient.java
@@ -0,0 +1,15 @@
+public class Patient {
+
+    int id;
+    String name;
+    int age;
+
+    Patient(int id, String name, int age) {
+
+        this.id = id;
+        this.name = name;
+        this.age = age;
+
+    }
+
+}
