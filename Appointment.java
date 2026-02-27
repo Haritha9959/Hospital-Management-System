b9294e4f3676a535cbd16d6d5cd2a7452698032e
Popuri Haritha <popuriharitha763@gmail.com>
2026-02-27 18:56:33 +0530

    Create Appointment.java

---
+++ Appointment.java
@@ -0,0 +1,15 @@
+public class Appointment {
+
+    int patientId;
+    int doctorId;
+    String date;
+
+    Appointment(int patientId, int doctorId, String date) {
+
+        this.patientId = patientId;
+        this.doctorId = doctorId;
+        this.date = date;
+
+    }
+
+}
