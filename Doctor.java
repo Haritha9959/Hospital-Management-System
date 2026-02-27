f93fac2e7292c2943954a540d9aecfe78d5fe6c4
Popuri Haritha <popuriharitha763@gmail.com>
2026-02-27 18:54:06 +0530

    Create Doctor.java

---
+++ Doctor.java
@@ -0,0 +1,15 @@
+public class Doctor {
+
+    int id;
+    String name;
+    String specialization;
+
+    Doctor(int id,String name,String specialization) {
+
+        this.id = id;
+        this.name = name;
+        this.specialization = specialization;
+
+    }
+
+}
