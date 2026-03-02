b0a2392c93cb654569abec42f4327297244b0c80
Popuri Haritha <popuriharitha763@gmail.com>
2026-03-02 13:32:53 +0530

    Refactor main method for role-based access control
    Removed the main menu loop and integrated role-based access control for user login.

--- Main.java
+++ Main.java
@@ -1,65 +1,16 @@
-import java.util.Scanner;
-
 public class Main {
-
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        HospitalManagement hm = new HospitalManagement();
+        String role = Login.loginUser();
 
-        int choice;
-
-        do {
-
-            System.out.println("\nHOSPITAL MANAGEMENT SYSTEM");
-            System.out.println("1 Add Patient");
-            System.out.println("2 View Patients");
-            System.out.println("3 Add Doctor");
-            System.out.println("4 View Doctors");
-            System.out.println("5 Book Appointment");
-            System.out.println("6 View Appointments");
-            System.out.println("7 Exit");
-
-            System.out.print("Enter Choice: ");
-            choice = sc.nextInt();
-
-            switch(choice) {
-
-                case 1:
-                    hm.addPatient();
-                    break;
-
-                case 2:
-                    hm.viewPatients();
-                    break;
-
-                case 3:
-                    hm.addDoctor();
-                    break;
-
-                case 4:
-                    hm.viewDoctors();
-                    break;
-
-                case 5:
-                    hm.bookAppointment();
-                    break;
-
-                case 6:
-                    hm.viewAppointments();
-                    break;
-
-                case 7:https://www.onlinegdb.com/online_java_compiler#editor_1
-                    System.out.println("Thank You");
-                    break;
-
-                default:
-                    System.out.println("Invalid Choice");
-
-            }
-
-        } while(choice != 7);
-
+        if (role == null) {
+            System.out.println("Invalid Login!");
+        } else if (role.equals("Admin")) {
+            Admin.menu();
+        } else if (role.equals("Doctor")) {
+            Doctor.menu();
+        } else if (role.equals("Receptionist")) {
+            Receptionist.menu();
+        }
     }
-
 }
